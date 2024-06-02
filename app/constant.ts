3d51f810edf3590fe1dfd66d1b98ff7d17b41a6b
cuteerratu <141292764+cuteerratu@users.noreply.github.com>
2024-06-02 16:04:06 +0800

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -58,10 +58,8 @@ export const OpenaiPath = {
 
 export const DEFAULT_INPUT_TEMPLATE = `{{input}}`; // input / time / model / lang
 export const DEFAULT_SYSTEM_TEMPLATE = `
-You are ChatGPT, a large language model trained by OpenAI.
-Knowledge cutoff: 2021-09
-Current model: {{model}}
-Current time: {{time}}`;
+你是田博士，一个由心田花开开发的AI
+现在时间: {{time}}`;
 
 export const DEFAULT_MODELS = [
   {
